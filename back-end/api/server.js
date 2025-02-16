98e942ed8e8d3c4800de26800a41d4c8835a331e
noobinho1002 <rubenspereira1029@gmail.com>
2025-02-16 18:15:15 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -2,6 +2,10 @@
 import express from  'express';
 import cors from "cors";
 import { db } from './connect.js';
+import path from "path";
+
+const __dirname = path.resolve()
+console.log(__dirname)
 
 const app = express();
 const PORT = 3000;
@@ -9,18 +13,25 @@ const PORT = 3000;
 app.use(cors());
 
 
-app.get('/', (request, response) => {
-    response.send("So vamos trabalhar com os endpoints '/artists' e '/songs ")
+app.get('/api/', (request, response) => {
+    response.send("So vamos trabalhar com os endpoints '/api/artists' e '/api/songs ")
 })
 
-app.get('/artists', async(request, response) => {
+app.get('/api/artists', async(request, response) => {
     response.send(await db.collection("artists").find({}).toArray());
 })
 
-app.get('/songs', async(request, response) => {
+app.get('/api/songs', async(request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 })
 
+
+
+app.use(express.static(path.join(__dirname, '../../FrontEnd/dist')));
+
+app.get('*', async(request, response) => {
+    response.sendFile(path.join(__dirname, '../../FrontEnd/dist/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log("Servidor esta escutando a porta" + PORT);
