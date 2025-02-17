52cbb1667171d6c31a820d9b5b205114d4afdd25
noobinho1002 <rubenspereira1029@gmail.com>
2025-02-16 21:17:45 -0300

    ajustes para o deploy

--- FrontEnd/api/api.js
+++ FrontEnd/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import "dotenv/config"
 
 const{ NODE_ENV } = process.env.NODE_ENV;
-const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
+const URL = "https://deploy-jornada-fullstack2.onrender.com/api";
 
 const responseArtists = await axios.get(URL + '/artists');
 const responseSongs = await axios.get(URL + '/songs');
